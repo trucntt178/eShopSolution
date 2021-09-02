1537ec9643e572ea2686cbae625e587578de2d11
truc nguyen <toruc1234@gmail.com>
2021-09-02 15:25:47 +0700

    Building the Application layer struture

---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalRecord { get; set; }
+
+    }
+}
