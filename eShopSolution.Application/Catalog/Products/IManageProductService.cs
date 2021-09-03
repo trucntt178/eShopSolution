d9cc900008bc1268545d64d26fb5c8534b0eab3e
truc nguyen <toruc1234@gmail.com>
2021-09-03 16:32:48 +0700

    Add View Count, Create, Delete, GetAllPaging

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -1,4 +1,5 @@
 ﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
 using eShopSolution.Application.Dtos;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,12 @@ namespace eShopSolution.Application.Catalog
     public interface IManageProductService
     {
         Task<int> Create(ProductCreateRequest request);
-        Task<int> Update(ProductEditRequest request);
+        Task<int> Update(ProductUpdateRequest request);
         Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+        Task AddViewCount(int productId);
         Task<List<ProductViewModel>> GetAll();
-        Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize);
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
     }
 }
--- eShopSolution.Application/Catalog/Products/ManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -1,11 +1,15 @@
 ﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
 using eShopSolution.Application.Dtos;
 using eShopSolution.Data.EF;
 using eShopSolution.Data.Entities;
+using eShopSolution.Utilities.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace eShopSolution.Application.Catalog.Products
 {
@@ -16,11 +20,36 @@ namespace eShopSolution.Application.Catalog.Products
         {
             _context = context;
         }
+
+        public async Task AddViewCount(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            product.ViewCount += 1;
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<int> Create(ProductCreateRequest request)
         {
             var product = new Product()
             {
-                Price = request.Price
+                Price = request.Price,
+                OriginalPrice = request.OriginalPrice,
+                Stock = request.Stock,
+                ViewCount = 0,
+                DateCreated = DateTime.Now,
+                ProductTranslations = new List<ProductTranslation>()
+                {
+                    new ProductTranslation()
+                    {
+                        Name = request.Name,
+                        Description = request.Description,
+                        Details = request.Details,
+                        SeoAlias = request.SeoAlias,
+                        SeoDescription = request.SeoDescription,
+                        SeoTitle = request.SeoTitle,
+                        LanguageId = request.LanguageId
+                    }
+                }
             };
             _context.Products.Add(product);
             return await _context.SaveChangesAsync();
@@ -28,7 +57,10 @@ namespace eShopSolution.Application.Catalog.Products
 
         public async Task<int> Delete(int productId)
         {
-            throw new NotImplementedException();
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new EShopException($"Cannot find a product: {productId}");
+            _context.Products.Remove(product);
+            return await _context.SaveChangesAsync();
         }
 
         public async Task<List<ProductViewModel>> GetAll()
@@ -36,12 +68,65 @@ namespace eShopSolution.Application.Catalog.Products
             throw new NotImplementedException();
         }
 
-        public async Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        {
+            //1. Select join
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        select new { p, pt, pic };
+            //2. Filter
+            if (!string.IsNullOrEmpty(request.Keyword))
+            {
+                query = query.Where(x => x.pt.Name.Contains(request.Keyword));
+            }
+
+            if (request.CategoryIds.Count > 0)
+            {
+                query = query.Where(x => request.CategoryIds.Contains(x.pic.CategoryId));
+            }
+            //3. Paging
+            int totalRow = await query.CountAsync();
+
+            var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(x => new ProductViewModel()
+                {
+                    Id = x.p.Id,
+                    Name = x.pt.Name,
+                    DateCreated = x.p.DateCreated,
+                    Description = x.pt.Description,
+                    Details = x.pt.Details,
+                    LanguageId = x.pt.LanguageId,
+                    OriginalPrice = x.p.OriginalPrice,
+                    Price = x.p.Price,
+                    SeoAlias = x.pt.SeoAlias,
+                    SeoDescription = x.pt.SeoDescription,
+                    SeoTitle = x.pt.SeoTitle,
+                    Stock = x.p.Stock,
+                    ViewCount = x.p.ViewCount
+                }).ToListAsync();
+            //4. Select and projection
+            var pagedResult = new PagedResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+            return pagedResult;
+        }
+
+        public async Task<int> Update(ProductUpdateRequest request)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<int> Update(ProductEditRequest request)
+        public Task<bool> UpdatePrice(int productId, decimal newPrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateStock(int productId, int addedQuantity)
         {
             throw new NotImplementedException();
         }
